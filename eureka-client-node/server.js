63cb3a223b373ec2454f6565356d127b88d25d24
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-09-26 16:22:39 -0400

    refined java and now for node

---
+++ eureka-client-node/server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const express = require('express');
+const Eureka = require('eureka-js-client').Eureka;
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// example configuration
+const eureka = new Eureka({
+	// application instance information
+	instance: {
+		app: 'a-node-service',
+		hostName: 'localhost',
+		ipAddr: '127.0.0.1',
+		statusPageUrl: 'http://localhost:3000',
+		vipAddress: 'jq.test.something.com',
+		port: {
+			'$': PORT,
+			'@enabled': 'true'
+		},
+		dataCenterInfo: {
+			'@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+			name: 'MyOwn',
+		},
+	},
+	eureka: {
+		// eureka server host / port
+		host: '127.0.0.1',
+		port: 8761,
+		servicePath: '/eureka/apps/'
+	},
+});
+
+eureka.logger.level('debug');
+eureka.start((error) => {
+	console.log(error || 'NodeJS Eureka Started!');
+});
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+	res.send('Hello from NodeJS Eureka Client\n');
+	res.end();
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
